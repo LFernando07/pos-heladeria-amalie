73023b3c1660ce71484b9e6910fd9ab281976bc7
JakePG16 <jpgjake03@gmail.com>
2025-10-08 08:23:27 -0600

    Aplicacion realizada hasta ticket con codigo de barra

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router'; // 1. Importa Link para la navegación
+import { FaPlusCircle } from 'react-icons/fa'; // Importa un icono
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-title">Heladería Amelie</h1>
+      {/* 2. Añade el botón envuelto en un Link */}
+      <Link to="/NuevoProducto" className="add-product-link">
+        <FaPlusCircle />
+        <span>Agregar Producto</span>
+      </Link>
+    </header>
+  );
+}
